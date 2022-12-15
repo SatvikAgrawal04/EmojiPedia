bc824ae44c3c0d9e7ead7abc570fd8bc3703d756
Satvik Agrawal <satwikagrawal@gmail.com>
2022-12-16 00:23:15 +0530

    added smiling face with tear emoji

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -1,32 +1,39 @@
 const emojipedia = [
-  {
-    id: 1,
-    emoji: "💪",
-    name: "Tense Biceps",
-    meaning:
-      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
-  },
-  {
-    id: 2,
-    emoji: "🙏",
-    name: "Person With Folded Hands",
-    meaning:
-      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
-  },
-  {
-    id: 3,
-    emoji: "🤣",
-    name: "Rolling On The Floor, Laughing",
-    meaning:
-      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
-  },
-  {
-    id: 4,
-    emoji: "😤",
-    name: "Face With Look of Triumph",
-    meaning:
-      "White steam clouds are coming out of the smiley's nose. Is snorting contemptuously as a sign of superiority. Is proud and triumphing over someone else."
-  }
+	{
+		id: 1,
+		emoji: "💪",
+		name: "Tense Biceps",
+		meaning:
+			"“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+	},
+	{
+		id: 2,
+		emoji: "🙏",
+		name: "Person With Folded Hands",
+		meaning:
+			"Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+	},
+	{
+		id: 3,
+		emoji: "🤣",
+		name: "Rolling On The Floor, Laughing",
+		meaning:
+			"This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+	},
+	{
+		id: 4,
+		emoji: "😤",
+		name: "Face With Look of Triumph",
+		meaning:
+			"White steam clouds are coming out of the smiley's nose. Is snorting contemptuously as a sign of superiority. Is proud and triumphing over someone else.",
+	},
+	{
+		id: 5,
+		emoji: "🥲",
+		name: "Smiling face with tear",
+		meaning:
+			"Smile, even in difficult times. Instead of being really happy, you smile despite the pain. The real feelings are hidden behind a smile. Someone is going through difficult times or has a lot of problems.",
+	},
 ];
 
 export default emojipedia;
